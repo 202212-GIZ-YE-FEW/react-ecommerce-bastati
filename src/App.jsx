9cb911975c630ddaa490f4db8dfe0c9137ac38b0
RuqaiahSaif <69811684+RuqaiahSaif@users.noreply.github.com>
2023-03-13 19:48:22 +0300

    feat:add all products

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css'
 import HomePage from './Pages/HomePage'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import ProductPage from "./Pages/ProductPage";
 function App() {
+  // Create a client
+  const queryClient = new QueryClient();
   return (
     <div classNameName="App">
+    
+      <QueryClientProvider client={queryClient}>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductPage />} />
       </Routes>
+      </QueryClientProvider>
     </div>
   )
 }
--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import { useQuery } from '@tanstack/react-query';
+import Axios from 'axios';
+import Card from '../components/Card';
+function ProductPage({ productId, productImage, productTitle, productPrice }) {
+  // Queries
+  const { data, isError, isFetching, isSuccess } = useQuery(['products'], () => {
+    return Axios.get('https://fakestoreapi.com/products')
+      .then(res => res.data)
 
-function ProductPage() {
+
+  });
   return (
-    <div>ProductPage</div>
+    <>
+      <div>ProductPage</div>
+      <div>{isFetching && 'Background Updating...'}</div>
+      <div>{isError && 'Error'}</div>
+      <div className="container">
+        <div className="row d-flex justify-content-evenly">
+
+
+          {isSuccess && data?.map(product =>
+          <Card productId={product.id}
+         productImage={product.image}
+              productTitle={product.title}
+              productPrice={product.price}
+          />
+           )}
+        </div>
+
+      </div>
+
+
+    </>
   )
 }
 
